bcef54ed1630914c83fac114b517947dfc2f2f28
soks-kat <marinacatalan12@gmail.com>
2026-05-11 09:42:39 +0200

    fixes in spacing and shi

--- about.js
+++ about.js
@@ -1,23 +1,23 @@
 const container = document.getElementById("container")
-container.style.display = 'none';
+// container.style.display = 'none';
 var in_container = false;
 window.addEventListener("keydown", function(event) {
     if (event.key == "Enter") {
         if (!in_container) {
-            container.style.display = "block";
+            container.showModal();
             in_container = true;
         } else {
-            container.style.display = "none";
+            container.close();
             in_container = false;
         }
     }
-    if (event.key == "Escape") {
-        container.style.display = "none";
-        in_container = false;
-    }
+    // if (event.key == "Escape") {
+    //     container.close();
+    //     in_container = false;
+    // }
 });
 
 document.getElementById("about").addEventListener("click", function() {
-    container.style.display = "block";
+    container.showModal();
     in_container = true;
 });
